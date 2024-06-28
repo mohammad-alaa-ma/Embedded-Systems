1d80ff3725814ef821559de794836a59abf40b74
Mohammad Alaa <mohammad.alaaeldin98@gmail.com>
2024-06-29 00:50:59 +0300

    Add MCU Interfacing

---
+++ Unit 8 (MCU Interfacing)/Lesson 6 (I2C)/Lab 1/HAL/MAX7221 Driver/MAX7221_private.h
@@ -0,0 +1,12 @@
+#ifndef MAX7221_PRIVATE_H_
+#define MAX7221_PRIVATE_H_
+
+/* The Register Address Map */
+#define Decode_Mode				0x09
+#define Intensity_Control		0x0A
+#define Scan_Limit				0x0B
+#define Display_Control			0x0C
+
+
+
+#endif	/* MAX7221_PRIVATE_H_ */
